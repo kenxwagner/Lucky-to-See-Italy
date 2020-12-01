a2b5ebe4ff63f594fb870b5bffa407cf663ac8d7
Ken Wagner <ken_xwagner@hotmail.com>
2020-11-30 23:56:31 -0800

    update

--- extra/carousel.js
+++ extra/carousel.js
@@ -2,3 +2,13 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
+
+class DemoCarousel extends Component {
+    render() {
+        return (
+            <Carousel>
+                <div>
+                    <img src="assets/1.jpeg" />
+                    <p className="legend">Legend 1</p>
+                </div>
+                <div>
